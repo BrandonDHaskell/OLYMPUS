cdfb4edffd94df6749377455203ac186da1958d8
BrandonDHaskell <BrandonDHaskell@outlook.com>
2023-11-20 23:32:02 -0800

    add folder structure and settings

---
+++ config/settings.py
@@ -0,0 +1,13 @@
+# config/settings.py
+
+# Paths
+USERS_PATH = "data/offline_json.json"
+LOG_PATH = "data/logs/olympus.log"
+
+# Logging Levels
+LOGGING_LEVEL = "INFO"  # Options: "DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"
+
+# User Clearance Levels
+USER_ADMIN = 1    # System admins for this service
+USER_MEMBER = 2   # Members of Noisebridge
+USER_GUEST = 3    # Guests of Noisebridge
