bfe41f68e92f675a4423885c4d8a917fbb688ae6
BrandonDHaskell <BrandonDHaskell@outlook.com>
2023-12-16 14:19:56 -0800

    update code to reader only

--- src/hardware_io/rfid_scanner.py
+++ src/hardware_io/rfid_scanner.py
@@ -8,17 +8,9 @@ ph = PasswordHasher()
 # Reads an RFID tag, hashes the ID and returns the hashed ID
 def get_hashed_id():
     try:
-        # Check for presence of RFID tag
-        (status, tag_type) = reader.request(reader.REQIDL)
-        
-        # if card is present, scan, else return nothing
-        if status == reader.OK:
-
-            id, text = reader.read()
-            hashed_id = ph.hash(str(id))
-            return hashed_id
-        else:
-            return None
+        id, text = reader.read()
+        hashed_id = ph.hash(str(id))
+        return hashed_id
     except Exception as e:
         # TODO: add logging
         print("Error: ", e)
