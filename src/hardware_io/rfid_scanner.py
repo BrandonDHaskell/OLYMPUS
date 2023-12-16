0361594ecb238e358455e0b609a807cc0bfe46ba
BrandonDHaskell <BrandonDHaskell@outlook.com>
2023-12-16 13:57:15 -0800

    update to stop scanning if no card is present

--- src/hardware_io/rfid_scanner.py
+++ src/hardware_io/rfid_scanner.py
@@ -8,8 +8,19 @@ ph = PasswordHasher()
 # Reads an RFID tag, hashes the ID and returns the hashed ID
 def get_hashed_id():
     try:
-        id, text = reader.read()
-        hashed_id = ph.hash(str(id))
-        return hashed_id
+        # Check for presence of RFID tag
+        (status, tag_type) = reader.request(reader.REQIDL)
+        
+        # if card is present, scan, else return nothing
+        if status == reader.OK:
+
+            id, text = reader.read()
+            hashed_id = ph.hash(str(id))
+            return hashed_id
+        else:
+            return None
+    except Exception as e:
+        # TODO: add logging
+        print("Error: ", e)
     finally:
         GPIO.cleanup()
