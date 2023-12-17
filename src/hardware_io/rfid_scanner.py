a56077a91106f815bca3d1713a1a97a28aabde8c
BrandonDHaskell <BrandonDHaskell@outlook.com>
2023-12-16 18:22:22 -0800

    update hash to sha3

--- src/hardware_io/rfid_scanner.py
+++ src/hardware_io/rfid_scanner.py
@@ -1,16 +1,18 @@
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
-from argon2 import PasswordHasher
+import hashlib
 
 reader = SimpleMFRC522()
-ph = PasswordHasher()
 
 # Reads an RFID tag, hashes the ID and returns the hashed ID
 def get_hashed_id():
     try:
         id, text = reader.read()
-        hashed_id = ph.hash(str(id))
-        return hashed_id
+        if (id):
+            hashed_id = hashlib.sha3_256(str(id).encode()).hexdigest()
+            return hashed_id
+        else:
+            return None
     except Exception as e:
         # TODO: add logging
         print("Error: ", e)
